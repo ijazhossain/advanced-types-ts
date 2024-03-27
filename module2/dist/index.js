8eda80d9dd41a51733933244bbc5b5611dea1174
ijazhossain <ijaz.hossain35@gmail.com>
2024-03-27 14:20:42 +0600

    project setup

---
+++ module2/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+{
+    const name = 'Ijaz Hossain';
+    console.log(name);
+    const greetMsg = 'Hello World';
+    console.log(greetMsg);
+}
---
+++ module2/src/index.ts
@@ -0,0 +1,5 @@
+{const name:string='Ijaz Hossain'
+console.log(name)
+const greetMsg='Hello World'
+console.log(greetMsg);
+}
