7f5d6a16fddf5b8f2f37dd7503ca3967b5a8978d
ijazhossain <ijaz.hossain35@gmail.com>
2024-03-31 11:47:41 +0600

    2.1 2.2 2.3 added

---
+++ module2/src/2.1.ts
@@ -0,0 +1,23 @@
+{
+    // type assertion
+    const kgToGm=(value:string|number):number|string|undefined=>{
+        if(typeof value ==='number'){
+            return value*1000;
+        }
+        if(typeof value === 'string'){
+            const convertedValue=parseFloat(value)*1000;
+            return `The converted value is : ${convertedValue}`
+        }
+    }
+    const result1 = kgToGm(1000)as number;
+    const result2 = kgToGm('1000')as string;
+
+    type CustomError={
+        message:string
+    }
+    try{
+
+    }catch(error){
+        console.log((error as CustomError).message);
+    }
+}
---
+++ module2/src/2.3.ts
@@ -0,0 +1,36 @@
+{
+    // Generic type
+    // const rollNumbers:number[]=[3,6,8];
+    type GenericArray<T>=Array<T>
+    const rollNumbers:GenericArray<number>=[3,6,8];
+    // const mentors:string[]=['Mr. X', 'Mr.Y', 'Mr.z'];
+    const mentors:GenericArray<string>=['Mr. X', 'Mr.Y', 'Mr.z'];
+    // const boolArray:boolean[]=[true, false, true];
+    const boolArray:GenericArray<boolean>=[true, false, true];
+
+// Generic for object
+    const user:GenericArray<{name:string;age:number}>=[
+        {
+            name:'Mezba',
+            age:100
+        },
+        {
+            name:'Jhonkar Mahbub',
+            age:110
+        }
+    ]
+
+    // Generic for tuple
+    type GenericTuple<X,Y>=[X,Y]
+    const person =['Mr.X', 'Mr. Y']
+
+    
+    const UserWithID:GenericTuple<number,{
+        name:string;
+        email: string;
+    }>=[1234,{
+        name:'Persian',
+        email:'a@gmail.com'
+    }]
+
+}
