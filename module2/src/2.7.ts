392d2c49599335615e0cea061ffd0efb3a9e536f
ijazhossain <ijaz.hossain35@gmail.com>
2024-04-17 10:35:24 +0600

    2.7.ts added

--- module2/src/2.7.ts
+++ module2/src/2.7.ts
@@ -1,14 +1,31 @@
 {
-    // 
+    // generic constraint with keyof operator
 
-
-
-
-
-
-
-
-
+type Vehicle ={
+    bike: string;
+    car: string;
+    ship: string;
+}
+type Owner = "bike" | "car" | "ship";
+type Owner2 = keyof Vehicle;
+const getPropertyValue=<X,Y extends keyof X>(obj:X, key:Y)=>{
+    return obj[key]
+}
+const user ={
+    name:'Mr. persian',
+    age: 26,
+    address:'ctg'
+}
+const car ={
+    model:'Mr. persian',
+    year: 1926,
     
+}
+const res1 = getPropertyValue(user,"name")
+
+
+
+
+
     // 
 }
